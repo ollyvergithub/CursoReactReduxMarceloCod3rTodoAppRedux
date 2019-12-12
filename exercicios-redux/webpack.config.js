8578cbc298c80f261f21e0cbb31e9fd2d9741ef1
Ollyver Ottoboni <ollyverottoboni@gmail.com>
2019-12-12 09:46:10 -0300

    Convertendo a aplicação para a versão do React 15.4.2, React-Dom 15.4.2, React-Redux 5.0.2 e Redux 3.6.0, ver package.json

---
+++ exercicios-redux/webpack.config.js
@@ -0,0 +1,26 @@
+const webpack = require('webpack')
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: __dirname + '/public',
+        filename: './bundle.js'
+    },
+    devServer: {
+        port: 8080,
+        contentBase: './public'
+    },
+
+    module: {
+        loaders: [{
+            test: /.js[x]?$/,
+            loader: 'babel-loader',
+            exclude: /node_modules/,
+            query: {
+                presets: ['es2015', 'react'],
+                // O plugin foi adicionado no ex.12
+                plugins: ['transform-object-rest-spread']
+            }
+        }]
+    }
+}
