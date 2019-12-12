d37cea72bbcce6e9e7397f65cccd81bf3d55b486
Ollyver Ottoboni <ollyverottoboni@gmail.com>
2019-12-12 16:04:03 -0300

    Aula 07 - 71 - Configurando os Reducers

---
+++ frontend/src/main/Reducers.js
@@ -0,0 +1,24 @@
+import {combineReducers} from "redux";
+
+const rootReducer = combineReducers({
+    todo: () => ({
+        description: 'Ler livro',
+        list: [
+            {
+            _id: 1,
+            description: "Pagar fatura do cartão",
+            done: true
+        },{
+            _id: 2,
+            description: "Reunião com equipe às 10:00",
+            done: false
+        },{
+            _id: 3,
+            description: "Compra o Vinil e Atibaia",
+            done: false
+        },
+        ]
+    })
+})
+
+export default rootReducer
